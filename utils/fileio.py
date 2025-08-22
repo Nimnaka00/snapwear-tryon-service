fd7635b6c48f8fe55798a3e09fad39978ee52a13
NImnaka <152169083+Nimnaka00@users.noreply.github.com>
2025-08-22 22:25:21 +0530

    Update fileio.py

--- utils/fileio.py
+++ utils/fileio.py
@@ -1,7 +1,8 @@
+# utils/fileio.py
 import shutil
 from fastapi import UploadFile
 
-async def save_upload_file(upload_file: UploadFile, destination: str):
+async def save_upload_file(upload_file: UploadFile, destination):
     with open(destination, "wb") as buf:
         shutil.copyfileobj(upload_file.file, buf)
     upload_file.file.close()
